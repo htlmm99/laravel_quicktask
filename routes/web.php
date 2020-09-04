f5ce90a731449666f830d6dc404fddcfc0f6ed85
Manh <hungyen1101@gmail.com>
2020-09-04 13:44:39 +0700

    change Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Task;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/task', function(){
+
+});
+
+Route::delete('task/{task}', function(Task $task){
+
 });
